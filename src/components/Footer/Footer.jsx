89114a9b6da0d66457f3eb64648703108a29ca97
Saurav Kumar <103248286+Saurav-K2002@users.noreply.github.com>
2024-05-20 10:59:10 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import { Avatar, Box, Stack, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
-
+const imgScr='https://avatars.githubusercontent.com/u/94047780?s=400&u=1a090cd2cc7d1c63049a10512ac83049e14ce520&v=4';
 
 const Footer = () => {
   return (
